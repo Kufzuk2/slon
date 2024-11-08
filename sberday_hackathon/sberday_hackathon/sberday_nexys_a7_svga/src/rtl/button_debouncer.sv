dd3d4c326b4a4e3a8132b3ef91d03d97c098aba3
KufzuK <vologin.vv@phystech.edu>
2024-11-08 17:05:47 +0300

    long button push implemented into debouncer

--- sberday_hackathon/sberday_hackathon/sberday_nexys_a7_svga/src/rtl/button_debouncer.sv
+++ sberday_hackathon/sberday_hackathon/sberday_nexys_a7_svga/src/rtl/button_debouncer.sv
@@ -1,13 +1,18 @@
 module button_debouncer #(
   parameter    CNT_WIDTH      = 16,
-  parameter    UNPUSHED_STATE = 1'b0
+  parameter    UNPUSHED_STATE = 1'b0,
+  parameter    HALF_MS        =  'd50000000
+
+
+
 )(
   input  wire     clk         ,
   input  wire     arst_n      ,
   input  wire     sw_i        ,
   output reg      sw_state_o  ,
   output reg      sw_down_o   ,
-  output reg      sw_up_o
+  output reg      sw_up_o     ,
+  output reg      sw_long
 );
 
 reg   [1:0] sw_r;
@@ -41,6 +46,25 @@ always @( posedge clk or negedge arst_n  ) begin
 always @(posedge clk) begin
   sw_down_o <= sw_change_f & sw_cnt_max & ~sw_state_o;
   sw_up_o   <= sw_change_f & sw_cnt_max &  sw_state_o;
+end
+
+localparam LONG_SIZE = $clog2(HALF_MS);
+reg [LONG_SIZE - 1: 0] long_cnt;
+
+
+always @(posedge clk) begin
+    if (!arst_n)
+        long_cnt <= 0;
+    else
+        long_cnt <= sw_state_o ? long_cnt + 1 : 0;
+end
+
+
+always @(posedge clk) begin
+    if (!arst_n)
+        sw_long <= 0;
+    else
+        sw_long <= (long_cnt == HALF_MS) ? 1: sw_long;
 end
 
 endmodule
