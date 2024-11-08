a33e369c18322af24b197a1ae074ce3fb6aa7d36
ErmolaevDmitrii <ermolaev.dn@phystech.edu>
2024-11-08 16:24:13 +0300

    [ermolaev] added random generator

---
+++ sberday_hackathon/sberday_hackathon/sberday_nexys_a7_svga/src/rtl/random_generator.sv
@@ -0,0 +1,25 @@
+module random_generator (
+    input logic clk,
+    
+    input logic [31:0] seed_i,
+    input logic        seed_v,
+
+    output logic [31:0] random_o
+);
+
+logic next_bit = random_o[31] ^
+                 random_o[30] ^
+                 random_o[29] ^
+                 random_o[27] ^
+                 random_o[25] ^
+                 random_o[ 0]; 
+
+always_ff @(posedge clk)
+    if (!rst)
+        random_o <= 32'h00000001;
+    if (seed_v)
+        random_o <= seed_i;
+    else
+        random_o <= {next_bit, random_o[31:1]};
+
+endmodule
